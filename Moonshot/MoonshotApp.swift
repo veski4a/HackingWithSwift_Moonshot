278e8fd39367fb0a4150ece8dd0d9c579c43cf6d
Veselin Stefanov <veski4a92@gmail.com>
2020-10-27 07:01:06 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Veselin Stefanov on 27.10.20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
